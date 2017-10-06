8d441a941e73f4f9e635cf0cc26959af5a332e34
JackReis01 <jackreis2019@gmail.com>
2017-10-06 08:55:16 -0500

    Final

--- Final/src/finalProject/BlackJack.java
+++ Final/src/finalProject/BlackJack.java
@@ -5,7 +5,14 @@ public class BlackJack {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Random rand = new Random();
+		int first = rand.nextInt(2) + 1;
+		String user;
+		do
+		{
+			Random rand = new Random();
+			int first = rand.nextInt(2) + 1;
+		}while("hit".equalsIgnoreCase(user))
 	}
 
 }
